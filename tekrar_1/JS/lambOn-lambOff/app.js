0d7659021a63f2c86b4d5cb6cf2aba5f91801e72
murttkapln <murttkapln@gmail.com>
2025-03-10 15:37:01 +0300

    bootstrap

--- tekrar_1/JS/lambOn-lambOff/app.js
+++ tekrar_1/JS/lambOn-lambOff/app.js
@@ -1,14 +1,36 @@
+const switchOn = document.querySelector(".btn-on");
+const switchOff = document.querySelector(".btn-off");
+const image = document.querySelector(".img");
+const text = document.querySelector("#text");
+const checkbox = document.querySelector("#checkbox");
+const h1 = document.createElement("h1");
+const text2 = document.createTextNode("Programlama Dilleri");
+const newH1 = document.querySelector(".new-h1");
 
+switchOn.addEventListener("click", () => {
+  image.src = "./assets/./images/switchOn.jpg";
+});
+switchOff.addEventListener("click", () => {
+  image.src = "./assets/./images/switchOff.jpg";
+});
 
-const switchOn = document.querySelector(".btn-on")
-const switchOff = document.querySelector(".btn-off")
-const image = document.querySelector(".img")
+text.addEventListener("keyup", () => {
+  checkbox.checked
+    ? (text.value = text.value.toUpperCase())
+    : (text.value = text.value.toLowerCase());
+});
 
+console.log(checkbox.checked);
+console.log(text.value);
 
-switchOn.addEventListener("click",()=>{
-    image.src="./assets/./images/switchOn.jpg";
-})
-switchOff.addEventListener("click",()=>{
-    image.src="./assets/./images/switchOff.jpg"
+//! Yeni bir elemean ekleme
+h1.appendChild(text2);
+document.body.appendChild(h1);
+h1.style.marginTop = "5%";
+h1.style.color = "red";
 
-})
+// ! new-h1 ekleme
+h1.appendChild(text2)
+newH1.appendChild(h1);
+newH1.style.marginTop = "5%";
+newH1.style.color = "red";
