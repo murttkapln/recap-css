2056c7f49a694eccf2b84555701c176b0bde7907
murttkapln <murttkapln@gmail.com>
2025-03-10 16:16:30 +0300

    DOM

--- tekrar_1/JS/lambOn-lambOff/app.js
+++ tekrar_1/JS/lambOn-lambOff/app.js
@@ -6,6 +6,10 @@ const checkbox = document.querySelector("#checkbox");
 const h1 = document.createElement("h1");
 const text2 = document.createTextNode("Programlama Dilleri");
 const newH1 = document.querySelector(".new-h1");
+const btnEkle = document.querySelector(".btn-ekle");
+const btnSil = document.querySelector(".btn-sil");
+const list = document.querySelector(".list");
+const input = document.querySelector(".input");
 
 switchOn.addEventListener("click", () => {
   image.src = "./assets/./images/switchOn.jpg";
@@ -30,7 +34,19 @@ h1.style.marginTop = "5%";
 h1.style.color = "red";
 
 // ! new-h1 ekleme
-h1.appendChild(text2)
+h1.appendChild(text2);
 newH1.appendChild(h1);
 newH1.style.marginTop = "5%";
 newH1.style.color = "red";
+
+// ! input ile yazılanları yeni bir liste olarak yazdırma Js ile
+
+btnEkle.addEventListener("click", () => {
+  const newList = document.createElement("li");
+  const textNode = document.createTextNode(input.value.toUpperCase());
+  newList.appendChild(textNode);
+  list.appendChild(newList);
+});
+btnSil.addEventListener("click", () => {
+  list.removeChild(list.lastElementChild);
+});
